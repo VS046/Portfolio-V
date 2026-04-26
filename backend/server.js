142dd348bd254e3ab51140a450e4160d87f0bba1
VS046 <vedikaswami946@gmail.com>
2026-04-26 23:07:57 +0530

    [FIX]:- added route

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 // Nodemailer Transporter Configuration
 const transporter = nodemailer.createTransport({
   service: "gmail",
