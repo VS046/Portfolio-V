a8f627499109163428ec8befa8a6e12e0f275755
VS046 <vedikaswami946@gmail.com>
2026-04-26 23:22:33 +0530

    connected backend to frontend

--- frontend/script.js
+++ frontend/script.js
@@ -252,7 +252,7 @@ document.addEventListener("DOMContentLoaded", () => {
       };
 
       try {
-        const response = await fetch("http://localhost:5000/api/contact", {
+        const response = await fetch("https://portfolio-v-69f9.onrender.com/api/contact", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
